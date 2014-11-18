ab042702e5b44659c5a6bc1403165c94331543a2
jordan.littlefair <jordanlittlefair@fsmail.net>
2014-11-18 18:57:15 +0000

    Task #1: Removed .dlls and shaders.

--- CopyDLLs.bat
+++ CopyDLLs.bat
@@ -2,12 +2,19 @@
 
 echo Copying Assimp32d.dll
 
-xcopy  "C:\Users\Jordan\Dropbox\Foundation\Assimp\bin\assimp_debug-dll_win32\Assimp32d.dll" "Foundation\" /y
-xcopy  "C:\Users\Jordan\Dropbox\Foundation\Assimp\bin\assimp_debug-dll_win32\Assimp32d.dll" "Bin\Win32\Debug\" /y
+xcopy  "Assimp\bin\assimp_debug-dll_win32\Assimp32d.dll" "Foundation\" /y
+xcopy  "Assimp\bin\assimp_debug-dll_win32\Assimp32d.dll" "Bin\Win32\Debug\" /y
 
 echo Copying Assimp32.dll
 
-xcopy  "C:\Users\Jordan\Dropbox\Foundation\Assimp\bin\assimp_release-dll_win32\Assimp32.dll" "Foundation\" /y
-xcopy  "C:\Users\Jordan\Dropbox\Foundation\Assimp\bin\assimp_release-dll_win32\Assimp32.dll" "Bin\Win32\Release\" /y
+xcopy  "Assimp\bin\assimp_release-dll_win32\Assimp32.dll" "Foundation\" /y
+xcopy  "Assimp\bin\assimp_release-dll_win32\Assimp32.dll" "Foundation\Engine" /y
+xcopy  "Assimp\bin\assimp_release-dll_win32\Assimp32.dll" "Bin\Win32\Release\" /y
+
+echo Copying d3dcompiler_46.dll
+
+xcopy "dlls\d3dcompiler_46.dll" "Foundation\" /y
+xcopy "dlls\d3dcompiler_46.dll" "Bin\Win32\Debug\" /y
+xcopy "dlls\d3dcompiler_46.dll" "Bin\Win32\Release\" /y
 
 pause
