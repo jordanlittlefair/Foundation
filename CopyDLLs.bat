fe18a428a296474b9483b2756ed99e30754bc6a6
jordan.littlefair <jordanlittlefair@fsmail.net>
2014-12-10 21:44:41 +0000

    Replace Assimp libraries with source code (3/3) 1. Delete existing assimp libraries 2. Add new Assimp source code + projects 3. Link Game with new libraries (Done in 3 steps to keep changes clear- was inserting incorrect renames.)

--- CopyDLLs.bat
+++ CopyDLLs.bat
@@ -1,16 +1,5 @@
 @echo off
 
-echo Copying Assimp32d.dll
-
-xcopy  "Assimp\bin\assimp_debug-dll_win32\Assimp32d.dll" "Foundation\" /y
-xcopy  "Assimp\bin\assimp_debug-dll_win32\Assimp32d.dll" "Bin\Win32\Debug\" /y
-
-echo Copying Assimp32.dll
-
-xcopy  "Assimp\bin\assimp_release-dll_win32\Assimp32.dll" "Foundation\" /y
-xcopy  "Assimp\bin\assimp_release-dll_win32\Assimp32.dll" "Foundation\Engine" /y
-xcopy  "Assimp\bin\assimp_release-dll_win32\Assimp32.dll" "Bin\Win32\Release\" /y
-
 echo Copying d3dcompiler_46.dll
 
 xcopy "dlls\d3dcompiler_46.dll" "Foundation\" /y
